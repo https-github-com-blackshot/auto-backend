f109046442523d2358dfd6de8b3708f3d1ee292f
tima-kh-97 <tima.kh.97@gmail.com>
2019-02-22 12:01:21 +0600

    core controller

--- core/src/main/java/kz/blackshot/auto/controller/CoreController.java
+++ core/src/main/java/kz/blackshot/auto/controller/CoreController.java
@@ -7,8 +7,10 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/core")
 public class CoreController {
 
     @Autowired
