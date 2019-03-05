e79b623a427e952528d6a8a8651ebf5ca3a06cbd
tima-kh-97 <tima.kh.97@gmail.com>
2019-03-05 22:19:05 +0600

    some fixes

--- core/src/main/java/kz/blackshot/auto/controller/CoreController.java
+++ core/src/main/java/kz/blackshot/auto/controller/CoreController.java
@@ -6,14 +6,11 @@ import kz.blackshot.auto.service.IUserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@Controller
+@RestController
 @RequestMapping("/core")
 public class CoreController {
 
